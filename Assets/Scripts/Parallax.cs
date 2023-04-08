0cd3f07f870484c04adaaccda1c57df205ff2007
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2023-04-08 18:38:40 -0400

    Parallax Effect Done
    Parallax effect is done, but I need to create a camera follow, going to use cinemachine

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    //multiplier used slow the rate at which the gameObject moves with camera's position
+    //the further back an object is, the larger their multiplier should be
+    //larger multiplier = faster movement with relation to camera's position
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+
     private Transform camTransform;
     private Vector3 lastCamPosition;
+    private float textureUnitSizeX;
+    //private float textureUnitSizeY;
     
     void Start()
     {
         camTransform = Camera.main.transform;
         lastCamPosition = camTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        //textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
     }
 
     void LateUpdate()
@@ -18,13 +29,24 @@ public class Parallax : MonoBehaviour
         //calculate camera's displacement
         Vector3 displacement = camTransform.position - lastCamPosition;
         
-        //modify gameobject's position based on camera's displacement
-        transform.position += displacement;
+        
+        //modify gameobject's position based on camera's displacement 
+        transform.position += new Vector3(displacement.x * parallaxEffectMultiplier.x,displacement.y * parallaxEffectMultiplier.y);
         
         //update lastCamPosition
         lastCamPosition = camTransform.position;
+
+        if (Mathf.Abs(camTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (camTransform.position.x - transform.position.x) % textureUnitSizeX; 
+            transform.position = new Vector3(camTransform.position.x + offsetPositionX, transform.position.y);
+        }
         
-        
-        
+        //vertical parallax effect 
+        // if (Mathf.Abs(camTransform.position.y - transform.position.y) >= textureUnitSizeY)
+        // {
+        //     float offsetPositionY = (camTransform.position.y - transform.position.y) % textureUnitSizeY; 
+        //     transform.position = new Vector3(transform.position.x, camTransform.position.y + offsetPositionY);
+        // }
     }
 }
