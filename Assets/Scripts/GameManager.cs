50b22481317f2972e91b4faf4069c4496f3a0d8d
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2023-05-05 22:13:11 -0400

    Update 2
    Game over, game complete, start and pause screens are implemented.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -57,6 +58,7 @@ public class GameManager : MonoBehaviour
         {
             //Game Over
             m_lives = 0;
+            SceneManager.LoadScene("GameOver");
         }
 
         if (value < 0)
