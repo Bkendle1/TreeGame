759370d719a46d6f8f3bd19b85973a63aef8361c
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2023-05-08 15:07:11 -0400

    Sound Update 1
    Added button sounds and bgm

--- Assets/Scripts/UI/ButtonSceneSwap.cs
+++ Assets/Scripts/UI/ButtonSceneSwap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,48 @@ using UnityEngine.SceneManagement;
 public class ButtonSceneSwap : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip sfx;
+    [Tooltip("How long to wait until button action executes.")]
+    [SerializeField] private float buttonActionDelay = .5f;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
-    public void SwapScene()
+    public void StartSwapScene()
+    {
+        audioSource.PlayOneShot(sfx);
+        Time.timeScale = 1f; // if the game is paused, unpause so invoke can play
+        Invoke("SwapScene", buttonActionDelay);
+    }
+
+    public void StartResumeGame()
+    {
+        Time.timeScale = 1f; // if the game is paused, unpause so invoke can play
+        audioSource.PlayOneShot(sfx);
+        Invoke("ResumeGame", buttonActionDelay);
+    }
+
+    private void ResumeGame()
+    {
+        gameObject.GetComponentInParent<PauseMenu>().ResumeGame();
+    }
+    
+    //swap scene on button press
+    private void SwapScene()
     {
         SceneManager.LoadScene(sceneName);
     }
+    
+    public void StartQuitGame()
+    {
+        Time.timeScale = 1f; // if the game is paused, unpause so invoke can play
+        audioSource.PlayOneShot(sfx);
+        Invoke("QuitGame", buttonActionDelay);
+    }
 
-    public void QuitGame()
+    private void QuitGame()
     {
         Application.Quit();
     }
